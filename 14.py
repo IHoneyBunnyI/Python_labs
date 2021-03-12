75f765eef9f9add4689123d2428b8a8f754bc922
Касим Байбиков <kasim.baybikov@gmail.com>
2021-03-13 00:19:50 +0300

    f14

--- 14.py
+++ 14.py
@@ -1,8 +1,8 @@
 from math import *
 
-def f4(n):
+def f14(n):
     if n == 0:
         return 10
     if n == 1:
         return 9
-    return (sin(f4(n - 1)) + (1/36) * f4(n -2))
+    return (sin(f14(n - 1)) + (1/36) * f14(n -2))
